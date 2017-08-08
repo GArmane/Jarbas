d389c5bf9122125c91ceb228d5d37ab26ff850df
Matheus Santos <msldasilva@gmail.com>
2017-08-07 21:32:20 -0300

    Configuração parcial Identity Server

---
+++ serverTCC/IdentityServer/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Security.Cryptography.X509Certificates;
+using IdentityServer.Utils;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+
+        public IConfigurationRoot Configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        }
+
+       
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            services.AddIdentityServer()
+                .AddSigningCredential(new X509Certificate2("cert.pfx"))
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients());
+                //.AddExtensionGrantValidator<GoogleGrantValidator>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseIdentityServer();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
---
+++ serverTCC/IdentityServer/Utils/Config.cs
@@ -0,0 +1,42 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+
+namespace IdentityServer.Utils
+{
+    public class Config
+    {
+        //Recursos protegidos, nesse caso, a API
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("jarbasApi", "Jarbas API")
+            };
+        }
+
+        //Clientes, quem pode se conectar ao Identity Server, nesse caso, o app do Ionic
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "jarbasApp",
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedGrantTypes = GrantTypes.List(new [] {GrantType.ResourceOwnerPassword, "googleAuth"}),
+                    AllowedScopes = {"jarbasApi", "offline_access"},
+                    AllowOfflineAccess = true,
+                    RefreshTokenUsage = TokenUsage.ReUse,
+                    RefreshTokenExpiration = TokenExpiration.Sliding,
+                    SlidingRefreshTokenLifetime = 60 * 60 * 24 * 7,
+                    AccessTokenLifetime = 60 * 60 * 24 * 1,
+                    AllowedCorsOrigins = {"http:localhost:8100"}
+                }
+            };
+        }
+    }
+}
