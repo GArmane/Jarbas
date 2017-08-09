4661d49ba50f92d6f8a757637abd43fac6c25aa1
Matheus Santos <msldasilva@gmail.com>
2017-08-09 17:49:43 -0300

    Correção em relacionamentos das models

--- serverTCC/serverTCC/Models/GrupoMovimentacoes.cs
+++ serverTCC/serverTCC/Models/GrupoMovimentacoes.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public string UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
     }
 }
