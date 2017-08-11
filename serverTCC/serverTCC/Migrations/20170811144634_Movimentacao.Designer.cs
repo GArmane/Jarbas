65b78507a083fc81e82c5145cb9ede07c030008a
Matheus Santos <msldasilva@gmail.com>
2017-08-11 11:48:03 -0300

    Migration para alterações na movimentação

--- serverTCC/serverTCC/Migrations/20170811144634_Movimentacao.Designer.cs
+++ serverTCC/serverTCC/Migrations/20170811144634_Movimentacao.Designer.cs
@@ -9,9 +9,10 @@ using serverTCC.Models;
 namespace serverTCC.Migrations
 {
     [DbContext(typeof(JarbasContext))]
-    partial class JarbasContextModelSnapshot : ModelSnapshot
+    [Migration("20170811144634_Movimentacao")]
+    partial class Movimentacao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn)
---
+++ serverTCC/serverTCC/Migrations/20170811144634_Movimentacao.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace serverTCC.Migrations
+{
+    public partial class Movimentacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movimentacao_AspNetUsers_UsuarioId",
+                table: "Movimentacao");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movimentacao_UsuarioId",
+                table: "Movimentacao");
+
+            migrationBuilder.DropColumn(
+                name: "Categoria",
+                table: "Movimentacao");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Movimentacao");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Categoria",
+                table: "Movimentacao",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UsuarioId",
+                table: "Movimentacao",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movimentacao_UsuarioId",
+                table: "Movimentacao",
+                column: "UsuarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movimentacao_AspNetUsers_UsuarioId",
+                table: "Movimentacao",
+                column: "UsuarioId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
