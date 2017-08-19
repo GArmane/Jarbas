6eaa853bfea70f881eb8a64b846174dd15521d0d
Giovanni Armane <giovanniarmane@gmail.com>
2017-08-18 21:36:40 -0300

    Implementação de método GetUser em ConfiguracoesController, correção de rotas

--- serverTCC/serverTCC/Controllers/ConfiguracoesController.cs
+++ serverTCC/serverTCC/Controllers/ConfiguracoesController.cs
@@ -1,11 +1,12 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using serverTCC.Data;
 using serverTCC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace serverTCC.Controllers
 {
@@ -21,124 +22,189 @@ namespace serverTCC.Controllers
             this._context = context;
         }
 
-        // Development test method only
+        /// <summary>
+        /// Retorna lista completa de configurações cadastradas.
+        /// Para fins de desenvolvimento apenas.
+        /// GET api/Configuracoes
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<Configuracoes> GetAll()
         {
             return _context.Configuracoes.ToList();
         }
 
-        // GET: api/Configuracoes/{id}
+        /// <summary>
+        /// Retorna uma configuração específica.
+        /// GET api/Configuracoes/{id}
+        /// </summary>
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int Id)
         {
-            var config = await _context.Configuracoes.FirstOrDefaultAsync(cfg => cfg.Id.Equals(Id));
+            try
+            {
+                var config = await _context
+                    .Configuracoes
+                    .FirstOrDefaultAsync(cfg => cfg.Id.Equals(Id));
 
-            if (config != null)
-            {
-                return Ok(config);
+                if (config != null)
+                {
+                    return Ok(config);
+                }
+                else
+                {
+                    ModelState.AddModelError("Configuracoes", "Configurações de usuário não encontradas.");
+                    return NotFound(ModelState.Values.SelectMany(v => v.Errors));
+                }
             }
-            else
+            catch (Exception e)
             {
-                ModelState.AddModelError(
-                    "Configuracoes", "Configurações de usuário não encontradas."
-                );
-                return NotFound(ModelState.Values.SelectMany(v => v.Errors));
+                return BadRequest(e.Message);
             }
         }
 
-        // POST: api/Configuracoes
+        /// <summary>
+        /// Retorna a configuração de usuário.
+        /// GET api/Configuracoes/Usuario/{id}
+        /// </summary>
+        [HttpGet("Usuario/{userId}")]
+        public async Task<IActionResult> GetUser([FromRoute] string userId)
+        {
+            try
+            {
+                var config = await _context
+                    .Configuracoes
+                    .FirstOrDefaultAsync(cfg => cfg.UsuarioId.Equals(userId));
+
+                return Ok(config);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Cria uma nova configuração de usuário.
+        /// POST api/Configuracoes
+        /// </summary>
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] Configuracoes configuracoes)
         {
-            var usuarioExists = await _context
-                .Users
-                .AnyAsync(u => u.Id.Equals(configuracoes.UsuarioId));
-            
-            if (usuarioExists)
+            try
             {
-                var usuarioHasConfig = await _context
-                    .Configuracoes
-                    .AnyAsync(cfg => cfg.UsuarioId.Equals(configuracoes.UsuarioId));
-
-                if(!usuarioHasConfig)
+                bool usuarioExists = await _context
+                    .Users
+                    .AnyAsync(u => u.Id.Equals(configuracoes.UsuarioId));
+            
+                if (usuarioExists)
                 {
-                    _context.Configuracoes.Add(configuracoes);
-                    _context.SaveChanges();
-                    return CreatedAtAction("Create", configuracoes);
+                    var usuarioHasConfig = await _context
+                        .Configuracoes
+                        .AnyAsync(cfg => cfg.UsuarioId.Equals(configuracoes.UsuarioId));
+
+                    if(!usuarioHasConfig)
+                    {
+                        _context.Configuracoes.Add(configuracoes);
+                        await _context.SaveChangesAsync();
+                        return CreatedAtAction("Create", configuracoes);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(
+                            "Configuracoes", "Configurações de usuário já existentes."
+                        );
+                        return BadRequest(ModelState.Values.SelectMany(v => v.Errors));
+                    }
                 }
                 else
                 {
                     ModelState.AddModelError(
-                        "Configuracoes", "Configurações de usuário já existentes."
-                    );
-                    return BadRequest(ModelState.Values.SelectMany(v => v.Errors));
-                }
-            }
-            else
-            {
-                ModelState.AddModelError(
-                    "Usuario", "Usuário não cadastrado no sistema."
-                );
-                return NotFound(ModelState.Values.SelectMany(v => v.Errors));
-            }
-        }
-
-        // PUT: api/Configuracoes/{id}
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Edit(
-            [FromRoute] string userId,
-            [FromBody] Configuracoes configuracoes)
-        {
-            var usuarioExists = await _context.Users.AnyAsync(u => u.Id.Equals(userId));
-            
-            if(usuarioExists) 
-            {    
-                var usuarioConfig = await _context
-                .Configuracoes
-                .FirstOrDefaultAsync(cfg => cfg.UsuarioId.Equals(userId));
-
-                if(usuarioConfig != null)
-                {
-                    usuarioConfig.Idioma = configuracoes.Idioma;
-                    _context.Configuracoes.Update(usuarioConfig);
-                    await _context.SaveChangesAsync();
-                    return Ok(usuarioConfig);
-                }
-                else
-                {
-                    ModelState.AddModelError(
-                        "Configurações", "Usuário não possui configurações definidas"
+                        "Usuario", "Usuário não cadastrado no sistema."
                     );
                     return NotFound(ModelState.Values.SelectMany(v => v.Errors));
                 }
             }
-            else 
+            catch(Exception e)
             {
-                ModelState.AddModelError(
-                    "Usuario", "Usuário não cadastrado no sistema."
-                );
-                return NotFound(ModelState.Values.SelectMany(v => v.Errors));
+                return BadRequest(e.Message);
             }
         }
 
-        // DELETE: api/Configuracores/{id}
-        [HttpDelete]
+        /// <summary>
+        /// Edita uma configuração existente.
+        /// PUT api/Configuracoes/{id}
+        /// </summary>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Edit([FromRoute] string userId, [FromBody] Configuracoes configuracoes)
+        {
+            try
+            {
+                var usuarioExists = await _context.Users.AnyAsync(u => u.Id.Equals(userId));
+            
+                if(usuarioExists) 
+                {    
+                    var usuarioConfig = await _context
+                    .Configuracoes
+                    .FirstOrDefaultAsync(cfg => cfg.UsuarioId.Equals(userId));
+
+                    if(usuarioConfig != null)
+                    {
+                        usuarioConfig.Idioma = configuracoes.Idioma;
+                        _context.Configuracoes.Update(usuarioConfig);
+
+                        await _context.SaveChangesAsync();
+                        return Ok(usuarioConfig);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(
+                            "Configurações", "Usuário não possui configurações definidas"
+                        );
+                        return NotFound(ModelState.Values.SelectMany(v => v.Errors));
+                    }
+                }
+                else 
+                {
+                    ModelState.AddModelError(
+                        "Usuario", "Usuário não cadastrado no sistema."
+                    );
+                    return NotFound(ModelState.Values.SelectMany(v => v.Errors));
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Deleta uma configuração existente.
+        /// DELETE api/Configuracoes/{id}
+        /// </summary>
+        [HttpDelete("{id}")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
-            var configuracoes = await _context.Configuracoes.FirstOrDefaultAsync(cfg => cfg.Id.Equals(id));
+            try
+            {
+                var configuracoes = await _context.Configuracoes.FirstOrDefaultAsync(cfg => cfg.Id.Equals(id));
 
-            if(configuracoes != null) 
-            {
-                _context.Configuracoes.Remove(configuracoes);
-                await _context.SaveChangesAsync();
-                return Ok();
+                if(configuracoes != null) 
+                {
+                    _context.Configuracoes.Remove(configuracoes);
+                    await _context.SaveChangesAsync();
+                    return Ok();
+                }
+                else
+                {
+                    ModelState.AddModelError(
+                        "Configuracoes", "Configurações não encontradas no sistemas."
+                    );
+                    return NotFound(ModelState.Values.SelectMany(v => v.Errors));
+                }
             }
-            else
+            catch (Exception e)
             {
-                ModelState.AddModelError(
-                    "Configuracoes", "Configurações não encontradas no sistemas."
-                );
-                return NotFound(ModelState.Values.SelectMany(v => v.Errors));
+                return BadRequest(e.Message);
             }
         }
     }
