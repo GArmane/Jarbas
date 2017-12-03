eb60529149b21a8a83decb09e4d640e7fcd3650d
Leonardo Costa <leonardoteck@gmail.com>
2017-12-03 02:47:29 -0200

    Gráfico em grupos de movimentações

--- client/www/js/controllers/listaGrupoMovCtrl.js
+++ client/www/js/controllers/listaGrupoMovCtrl.js
@@ -11,11 +11,14 @@
         var vm = this;
 
         vm.dados = [];
+        vm.movimentacoes = [];
         vm.grupo = new GrupoMovimentacoes();
         vm.grupo.nome = '';
-
+        
         vm.add = add;
         vm.alterar = alterar;
+        
+        var grafico;
 
         activate();
 
@@ -175,20 +178,104 @@
             });
         }
 
+        function criarGrafico() {
+            var ctx = document.getElementById("graficoGrupos");
+            grafico = new Chart(ctx, {
+                type: 'pie',
+                data: {
+                    datasets: [{
+                        label: 'Valor acumulado',
+                        backgroundColor: vm.dados.map(function (grupo, i) {
+                            return utilities.getColor(i, true);
+                        }),
+                        borderColor:  vm.dados.map(function (grupo, i) {
+                            return utilities.getColor(i, false);
+                        }),
+                        data: vm.dados.map(function (grupo) {
+                            return grupo.valor;
+                        }),
+                        borderWidth: vm.dados.map(function (grupo) {
+                            return 4;
+                        })
+                    }],
+                    labels: vm.dados.map(function (grupo) {
+                        return grupo.nome;
+                    })
+                },
+                options: {
+                    responsive: true,
+                    title: {
+                        display: false
+                    },
+                    layout: {
+                        padding: {
+                            left: 50,
+                            right: 50,
+                            top: 50,
+                            bottom: 50
+                        }
+                    }
+                }
+            });
+        }
+
         function carregarDados() {
-            if (utilities.online())
+            if (utilities.online()) {
                 $http({
                     method: 'GET',
                     url: api.url() + 'GrupoMovimentacoes/Usuario/' + auth.id,
                     headers: auth.header
                 }).success(success)
                 .error(utilities.apiError);
-            else
+                $http({
+                    url: api.url() + 'Movimentacoes/Usuario/' + auth.id,
+                    method: 'GET',
+                    headers: auth.header
+                }).success(function (data) {
+                    vm.movimentacoes = data.movimentacoes;
+                    associaGrupoMov();
+                }).error(utilities.apiError);
+            } else {
                 localEntities.getAll('GrupoMovimentacoes').then(success);
+                localEntities.getAll('Movimentacao').then(function (data) {
+                    vm.movimentacoes = data;
+                    localEntities.getAll('Transferencia').then(function (data) {
+                        data.forEach(function(transf) {
+                            for (var i = 0; i < vm.movimentacoes.length; i++) {
+                                var mov = vm.movimentacoes[i];
+                                if (transf.receitaId == mov.id || transf.despesaId == mov.id)
+                                    vm.movimentacoes.splice(i--, 1);
+                            }
+                        }, this);
+                        associaGrupoMov();
+                    });
+                });
+            }
 
             function success(data) {
                 vm.dados = data;
+                associaGrupoMov();
             }
+        }
+
+        function associaGrupoMov() {
+            if (vm.movimentacoes.length == 0 || vm.dados.length == 0)
+                return;
+            for (var i = 0; i < vm.movimentacoes.length; i++) {
+                var mov = vm.movimentacoes[i];
+                if (mov.tipoMovimentacao == 0)
+                    vm.movimentacoes.splice(i--, 1);
+                else
+                    for (var j = 0; j < vm.dados.length; j++) {
+                        var grupo = vm.dados[j];
+                        if (grupo.id == mov.grupoMovimentacoesId) {
+                            if (!grupo.valor)
+                                grupo.valor = 0;
+                            grupo.valor += mov.valor;
+                        }
+                    }
+            }
+            criarGrafico();
         }
     }
 })();
--- client/www/js/services/utilitiesServices.js
+++ client/www/js/services/utilitiesServices.js
@@ -12,10 +12,56 @@
         this.apiError = apiError;
         this.isApp = isApp;
         this.online = online;
+        this.getColor = getColor;
 
         var _isApp = false;
         var isOnline = true;
         
+        var colors = [
+            '#3366CC',
+            '#DC3912',
+            '#FF9900',
+            '#109618',
+            '#990099',
+            '#3B3EAC',
+            '#0099C6',
+            '#DD4477',
+            '#66AA00',
+            '#B82E2E',
+            '#316395',
+            '#994499',
+            '#22AA99',
+            '#AAAA11',
+            '#6633CC',
+            '#E67300',
+            '#8B0707',
+            '#329262',
+            '#5574A6',
+            '#3B3EAC'
+        ];
+        var transps = [
+            'rgba(51,	102,	204,    0.6)',
+            'rgba(220,	57,	    18,     0.6)',
+            'rgba(255,	153,	0,      0.6)',
+            'rgba(16,	150,	24,     0.6)',
+            'rgba(153,	0,	    153,    0.6)',
+            'rgba(59,	62,	    172,    0.6)',
+            'rgba(0,	153,	198,    0.6)',
+            'rgba(221,	68,	    119,    0.6)',
+            'rgba(102,	170,	0,      0.6)',
+            'rgba(184,	46,	    46,     0.6)',
+            'rgba(49,	99,	    149,    0.6)',
+            'rgba(153,	68,	    153,    0.6)',
+            'rgba(34,	170,	153,    0.6)',
+            'rgba(170,	170,	17,     0.6)',
+            'rgba(102,	51,	    204,    0.6)',
+            'rgba(230,	115,	0,      0.6)',
+            'rgba(139,	7,	    7,      0.6)',
+            'rgba(50,	146,	98,     0.6)',
+            'rgba(85,	116,	166,    0.6)',
+            'rgba(59,	62,	    172,    0.6)'
+        ];
+
         activate();
 
         ////////////////
@@ -63,6 +109,12 @@
 
         function online() {
             return isOnline && navigator.onLine;
+        }
+
+        function getColor(i, transp) {
+            if (transp)
+                return transps[i];
+            return colors[i % 20];
         }
     }
 })();
