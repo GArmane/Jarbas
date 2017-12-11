508cc1d2a17b2bed04ebca66003cb2a48b04c206
Matheus Santos <msldasilva@gmail.com>
2017-12-10 23:59:58 -0200

    Migrations de teste

---
+++ serverTCC/IdentityServer/Migrations/20171211004819_addTokenDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityServer.Migrations
+{
+    public partial class addTokenDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
